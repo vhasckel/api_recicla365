6af1db4314ef3bd76b84d8abb5dc616931123a28
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-07-30 21:46:03 -0300

    feat: CRUD completo para rota de pontos de coleta

--- src/routes/recyclingPoints.routes.js
+++ src/routes/recyclingPoints.routes.js
@@ -4,5 +4,9 @@ const RecyclingPointController = require("../controllers/RecyclingPointControlle
 const recyclingRoutes = new Router();
 
 recyclingRoutes.post("/cadastrar", RecyclingPointController.createCollectPoint);
+recyclingRoutes.get("/", RecyclingPointController.getAllRecyclingPoints);
+recyclingRoutes.get("/:id", RecyclingPointController.getOneRecyclingPoint);
+recyclingRoutes.put("/:id", RecyclingPointController.updateRecyclingPoint);
+recyclingRoutes.delete("/:id", RecyclingPointController.deleteRecyclingPoint);
 
 module.exports = recyclingRoutes;
