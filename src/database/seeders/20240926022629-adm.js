8672cf88b8feebdfdaae3c8b3d1ac242df613caf
vanessa hasckel <vanessahasckel00@hotmail.com>
2024-09-25 23:36:04 -0300

    fix: corrigido a associação da tabela de permissões com a tabela de usuários

--- src/database/seeders/20240926022629-adm.js
+++ src/database/seeders/20240926022629-adm.js
@@ -1,12 +1,13 @@
 "use strict";
 const bcrypt = require("bcryptjs");
+const Permission = require("../../models/Permission");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     const hashedPassword = await bcrypt.hash("admin123", 10);
 
-    return queryInterface.bulkInsert(
+    const users = await queryInterface.bulkInsert(
       "users",
       [
         {
@@ -21,7 +22,6 @@ module.exports = {
           number: 1,
           birthdate: "2000-01-01",
           email: "admin@email.com",
-          permission: "adm",
           passwordHash: hashedPassword,
           createdAt: new Date(),
           updatedAt: new Date(),
@@ -29,9 +29,29 @@ module.exports = {
       ],
       { returning: true }
     );
+
+    // obter o ID da permissão "adm"
+    const adminPermission = await Permission.findOne({
+      where: { description: "adm" },
+    });
+
+    if (adminPermission) {
+      // associar o usuário Admin à permissão "adm"
+      await queryInterface.bulkInsert("user_permissions", [
+        {
+          userId: users[0].id,
+          permissionId: adminPermission.id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]);
+    } else {
+      console.error("Permissão 'adm' não encontrada.");
+    }
   },
 
   async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("user_permissions", { userId: 1 });
     return queryInterface.bulkDelete("users", { email: "admin@email.com" });
   },
 };
